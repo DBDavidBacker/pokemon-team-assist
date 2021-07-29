8433e774b6d65e8e17a0f00fb2cd42789c99a8f4
David <dbdavidbacker@gmail.com>
2021-07-29 15:00:56 -0400

    Updated move table sql

---
+++ db/moveTable.js
@@ -0,0 +1,16 @@
+let con = require('./index.js');
+
+con.connect((err) => {
+  if (err) {
+    throw err;
+  }
+
+  let sql = 'CREATE TABLE moves (name VARCHAR(40), id INT(3) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT, formatName VARCHAR (40), type VARCHAR(9), kind VARCHAR(9), PP INT(2), power INT(3), accuracy INT(3) )';
+
+  con.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log('abilities created');
+  });
+})
